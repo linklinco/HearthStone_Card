bda72f7ebb09aa2e0684178f2f6ba4706ac7967a
yollose <yollose@163.com>
2018-12-20 22:41:09 +0800

    v0.1

---
+++ __main__.py
@@ -0,0 +1,73 @@
+import time
+import requests
+import os
+import json
+import urllib.request
+
+class form():
+    def __init__(self,cardClass,p = 1,stand=0):
+        self.cardClass = cardClass
+        self.p = p
+        self.standard = stand
+        self.totalCard = []
+    def now_timec(self):
+        return int(round(time.time() * 1000))
+    def get_all(self):
+        all_num = self.get_url()
+        for i in range(2,all_num+1):
+            self.p = i
+            self.get_url()
+    def get_url(self):
+        data = {
+            "cardClass": self.cardClass,
+            "p": self.p,
+            "standard": self.standard,
+            "keywords": "",
+            "t": self.now_timec()
+        }
+        header = {
+            "Host": "hs.blizzard.cn",
+            "Proxy-Connection": "keep-alive",
+            "Content-Length": "56",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Origin": "http://hs.blizzard.cn",
+            "X-Requested-With": "XMLHttpRequest",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36",
+            "DNT": "1",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer": "http://hs.blizzard.cn/cards/",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9"
+        }
+        url = "http://hs.blizzard.cn/action/cards/query"
+        response = requests.post(url, data, headers=header)
+        cards = response.json()['cards']
+        pagesize = response.json()['totalPage']
+        for card in cards:
+            des = {}
+            des['花费'] = card['cost']
+            des['卡名'] = card['name']
+            des['稀有程度'] = card['cardRarity']
+            des['拓展包'] = card['cardSet']
+            des['卡牌类型'] = card['cardType']
+            des['卡牌背景']=card['background']
+            des['卡牌描述']=card['description']
+            des['卡牌链接']=card['imageUrl'].strip('\n')
+            self.totalCard.append(des)
+        return pagesize
+if __name__ == "__main__":
+    cardClass = ["druid", "hunter", "mage", "neutral", "paladin", "priest", "rogue", "shaman", "warlock", "warrior"]
+    for i in cardClass:
+        a = form(i)
+        a.get_all()
+        tc = a.totalCard
+        #创建文件夹保存图片
+        os.mkdir(i)
+        for j in tc:
+            path ='%s\\%s.png'%(i,j['卡名'])
+            data=urllib.request.urlopen(j['卡牌链接'])
+            with open(path,'wb') as f1:
+                f1.write(data.read())
+        data = json.dumps(a.totalCard,ensure_ascii=False)
+        with open(i+'\\'+i+'.json','w',encoding='utf8') as f:
+            f.write(data)
---
+++ readme.md
@@ -0,0 +1,37 @@
+##  炉石传说爬虫
+
+从暴雪网站爬取所有的炉石卡牌，并且下载图片.
+
+### 运行
+```
+#需要requests库
+python __mian__.py
+```
+
+### 可以添加的选项
+
+`attack`：攻击力
+
+`cost`：费用
+
+`health`：生命值
+
+`background`：背景
+
+`cardRarity`：稀有程度
+
+`cardEffect`：卡牌效果
+
+`cardSet`：卡牌集合
+
+`cardType`：卡牌类型
+
+`code`：名称（英文）
+
+`name`：名称中文
+
+`desctiption`：卡牌描述
+
+`imageUrl`：卡牌图片地址
+
+`durability`：武器耐久度
